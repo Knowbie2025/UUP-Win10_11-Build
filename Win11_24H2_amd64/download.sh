70526504e1289c10b099af39546ba477a0508f18
Knowbie2025 <gj287990776@gmail.com>
2026-05-30 20:18:35 +0800

    Update26100.8524 download.sh

--- Win11_24H2_amd64/download.sh
+++ Win11_24H2_amd64/download.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-UUP_ID=15127f8d-75c8-4150-98d1-5020aa815a35
+UUP_ID=7cb3bf8e-cba4-4e21-a089-5787a76fd839
 curl -Ls https://uupdump.net/get.php?id=${UUP_ID}&pack=esd&lang=zh-cn&edition=all > aria2.txt
 aria2c -x16 -s16 -j8 -c -i aria2.txt
