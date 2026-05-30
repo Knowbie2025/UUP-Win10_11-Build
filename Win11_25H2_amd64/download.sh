a19183af0c98db126aeb5c6555dfdd3e5314bed1
Knowbie2025 <gj287990776@gmail.com>
2026-05-30 20:31:00 +0800

    Delete Win11_25H2_amd64 directory

--- Win11_25H2_amd64/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-UUP_ID=b4d8466a-0024-4453-a0ed-1e0b9f7793d3
-curl -Ls https://uupdump.net/get.php?id=${UUP_ID}&pack=esd&lang=zh-cn&edition=all > aria2.txt
-aria2c -x16 -s16 -j8 -c -i aria2.txt
