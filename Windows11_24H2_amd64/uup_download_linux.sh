cb92550a8ee1770273ff9c3d7ed792cf746ec1f3
Knowbie2025 <auto-build@github@example.com>
2026-07-16 14:09:33 +0000

    Auto replace folder by uploaded zip

--- Windows11_24H2_amd64/uup_download_linux.sh
+++ Windows11_24H2_amd64/uup_download_linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Generated on 2026-07-03 17:11:21 GMT
+#Generated on 2026-07-16 13:47:49 GMT
 
 # Proxy configuration
 # If you need to configure a proxy to be able to connect to the internet,
@@ -47,7 +47,7 @@ fi
 
 echo ""
 echo "Retrieving aria2 script for the UUP set..."
-aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=fe1f1547-38fb-4867-a152-ddec85a47b37&pack=zh-cn&edition=professional%3Bcorecountryspecific&aria2=2"
+aria2c --no-conf --console-log-level=warn --log-level=info --log="aria2_download.log" -o"$tempScript" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=57ee5844-e735-4dc8-a764-66b257e03b5b&pack=zh-cn&edition=professional%3Bcorecountryspecific&aria2=2"
 if [ $? != 0 ]; then
   echo "Failed to retrieve aria2 script"
   exit 1
--- Windows11_24H2_amd64/uup_download_windows.cmd
+++ Windows11_24H2_amd64/uup_download_windows.cmd
@@ -1,5 +1,5 @@
 @echo off
-:: Generated on 2026-07-03 17:11:21 GMT
+:: Generated on 2026-07-16 13:47:49 GMT
 
 :: Proxy configuration
 :: If you need to configure a proxy to be able to connect to the internet,
@@ -52,7 +52,7 @@ SETLOCAL DISABLEDELAYEDEXPANSION
 goto :EOF
 
 :START_PROCESS
-title 26100.8737_amd64_zh-cn_multi_fe1f1547 download
+title 26100.8875_amd64_zh-cn_multi_57ee5844 download
 
 set "aria2=files\aria2c.exe"
 set "a7z=files\7zr.exe"
@@ -79,7 +79,7 @@ echo.
 
 :DOWNLOAD_APPS
 echo Retrieving aria2 script for Microsoft Store Apps...
-"%aria2%" --no-conf --async-dns=false --console-log-level=warn --log-level=info --log="aria2_download.log" -o"%aria2Script%" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=fe1f1547-38fb-4867-a152-ddec85a47b37&pack=neutral&edition=app&aria2=2"
+"%aria2%" --no-conf --async-dns=false --console-log-level=warn --log-level=info --log="aria2_download.log" -o"%aria2Script%" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=57ee5844-e735-4dc8-a764-66b257e03b5b&pack=neutral&edition=app&aria2=2"
 if %ERRORLEVEL% GTR 0 call :DOWNLOAD_ERROR & exit /b 1
 echo.
 
@@ -99,7 +99,7 @@ echo.
 
 :DOWNLOAD_UUPS
 echo Retrieving aria2 script for the UUP set...
-"%aria2%" --no-conf --async-dns=false --console-log-level=warn --log-level=info --log="aria2_download.log" -o"%aria2Script%" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=fe1f1547-38fb-4867-a152-ddec85a47b37&pack=zh-cn&edition=professional%%3Bcorecountryspecific&aria2=2"
+"%aria2%" --no-conf --async-dns=false --console-log-level=warn --log-level=info --log="aria2_download.log" -o"%aria2Script%" --allow-overwrite=true --auto-file-renaming=false "https://uupdump.net/get.php?id=57ee5844-e735-4dc8-a764-66b257e03b5b&pack=zh-cn&edition=professional%%3Bcorecountryspecific&aria2=2"
 if %ERRORLEVEL% GTR 0 call :DOWNLOAD_ERROR & exit /b 1
 echo.
 
